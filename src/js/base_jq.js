9bcdc768c81e957f1323bfc314c5f949370fa8ea
Ilya-via <ilya95v@gmail.com>
2020-01-24 06:00:51 +0300

    preloader

--- src/js/base_jq.js
+++ src/js/base_jq.js
@@ -1,3 +1,11 @@
+//PRELOADER
+$(window).on('load', function () {
+    $preloader = $('.loaderArea'),
+        $loader = $preloader.find('.loader');
+    $loader.fadeOut();
+    $preloader.delay(300).fadeOut('slow');
+});
+
 var eventObj = {
     appended: ""
 };
